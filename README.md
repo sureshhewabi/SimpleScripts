d9172d925ef004ae588bfce7da0b073f1c40c42d
Suresh Hewapathirana <hewapathirana@ebi.ac.uk>
2018-11-07 14:59:16 +0000

    Add more

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # SimpleScripts
 various file handing scripts for day to day work
+
+## Bash Scripts
+
+- replacewithoutUnderscore.sh
+
+## Python Scripts
+
+- replaceText.py
--- replaceText.py
+++ replaceText.py
@@ -1,19 +1,40 @@
 #!/usr/bin/env python3
+#
+# Replaces all the text with new text and save into a new file
+#
+# Input: This is the sample text with the word old word and I want to replace old word with new word
+# Output: This is the sample text with the word new word and I want to replace new word with new word
+#
+# Format python3 replaceText.py -inf  <input file> -o "old" -n "new" -outf <output file>
+# Example1: python3 replaceText.py -inf in.txt -o "old word" -n "new word" -outf out.txt
+#
+# Author: Suresh Hewapathirana
 
-filein = '<input file path>'
-fileout = '<output file path>'
-old_data = 'old_data'
-new_data = 'new'
+import argparse
 
-# Read the input file and copy it to the memory
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-inf", "--infile", required=True, help="Input filenamer")
+ap.add_argument("-o", "--oldtext", required=True, help="The text that is need to be replaced")
+ap.add_argument("-n", "--newtext", required=True, help="The new text that will be replaced")
+ap.add_argument("-outf", "--outfile", required=True, help="Output filename")
+args = vars(ap.parse_args())
+
+# assign values of the arguments
+filein = args["infile"]
+fileout = args["outfile"]
+old_data =args["oldtext"]
+new_data = args["newtext"]
+
+print("Starting reading the input file..")
 f = open(filein,'r')
 filedata = f.read()
 f.close()
 
-# Replace the old data with new ones
+print("Replacing the old data with new ones...")
 newdata = filedata.replace(old_data, new_data)
 
-# Write it to a new file
+print("Writting to a new file...")
 f = open(fileout,'w')
 f.write(newdata)
 f.close()
